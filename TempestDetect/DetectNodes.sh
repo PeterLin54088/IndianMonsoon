bc4f04aa3a0ba6db7d74db64333d40967abbd217
HSUAN-CHENG LIN <r12229008@ntu.edu.tw>
2024-06-13 17:20:58 +0800

    style: Modify TC Counter output name
    Add pressure information postfix to output csv

--- TempestDetect/DetectNodes.sh
+++ TempestDetect/DetectNodes.sh
@@ -1,10 +1,13 @@
+plevel=925
+svo_limit=0.00003
+
 DetectNodes \
---in_data "/work/b08209033/preprocess_ERA5/preprocess_MSLP.nc;/work/b08209033/preprocess_ERA5/preprocess_Vorticity_925.nc" \
---out "TropicalCycloneCount.csv" \
+--in_data "/work/b08209033/DATA/ERA5/slp/slp_alltime.nc;/work/b08209033/DATA/ERA5/div_vor/test.nc" \
+--out "TropicalCycloneCount_${plevel}.csv" \
 --searchbymin msl \
 --mergedist 5 \
---closedcontourcmd "msl,100,5,0;svo,-0.00003,5,0" \
---thresholdcmd "msl,<=,100400,0;svo,>=,0.00003,0" \
+--closedcontourcmd "msl,100,5,0;svo,-${svo_limit},5,0" \
+--thresholdcmd "msl,<=,100400,0;svo,>=,${svo_limit},0" \
 --outputcmd "msl,min,0;svo,max,0" \
 --minlon 45 \
 --maxlon 100 \
